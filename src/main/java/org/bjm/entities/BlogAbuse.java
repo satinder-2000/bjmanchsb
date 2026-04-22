c8ffda50d8a55d6da333587f10f97eb7a8dbb291
satinder-2000 <satinder_2000@outlook.com>
2026-04-23 05:08:32 +0530

    Entities and Repositories

--- src/main/java/org/bjm/entities/BlogAbuse.java
+++ src/main/java/org/bjm/entities/BlogAbuse.java
@@ -2,10 +2,12 @@ package org.bjm.entities;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class BlogAbuse {
 	
 	@Id
