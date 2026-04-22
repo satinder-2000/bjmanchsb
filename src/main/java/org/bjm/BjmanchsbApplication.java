db4507fbfaf67dcc89fe86d86f4eb3f8d3930eb9
satinder-2000 <satinder_2000@outlook.com>
2026-04-22 09:40:35 +0530

    first commit

---
+++ src/main/java/org/bjm/BjmanchsbApplication.java
@@ -0,0 +1,13 @@
+package org.bjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjmanchsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjmanchsbApplication.class, args);
+	}
+
+}
